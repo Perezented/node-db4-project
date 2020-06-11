920c95b207ddac41925789d67be1418bde3c2e7d
Michael Perez <mperez6473@gmail.com>
2020-06-11 17:29:24 -0500

    Added the Recipe for each recipe

--- data/seeds/003Recipe.js
+++ data/seeds/003Recipe.js
@@ -1,13 +1,16 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function (knex) {
+    // Deletes ALL existing entries
+    return knex("Recipe")
+        .del()
+        .then(function () {
+            // Inserts seed entries
+            return knex("Recipe").insert([
+                { RecipeID: 1, IngredientsID: "1", Quantity: "5oz" },
+                { RecipeID: 1, IngredientsID: "2", Quantity: "1 pinch" },
+                { RecipeID: 2, IngredientsID: "1", Quantity: "6oz" },
+                { RecipeID: 2, IngredientsID: "4", Quantity: "4-5 Cubes" },
+                { RecipeID: 3, IngredientsID: "1", Quantity: "8oz" },
+                { RecipeID: 3, IngredientsID: "3", Quantity: "2 minutesss" },
+            ]);
+        });
 };
