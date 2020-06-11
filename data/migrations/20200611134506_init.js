eb443af3bc7263fadf590d0ad4e830eec2dcb79d
Michael Perez <mperez6473@gmail.com>
2020-06-11 13:57:50 -0500

    Added a table for a single recipe and steps for the recipe. Have them on the same Migration that has not been merged yet.

--- data/migrations/20200611134506_init.js
+++ data/migrations/20200611134506_init.js
@@ -7,11 +7,36 @@ exports.up = function (knex) {
         .createTable("Ingredients", (tbl) => {
             tbl.increments();
             tbl.string("Ingredient").notNullable().index();
+        })
+        .createTable("Recipe", (tbl) => {
+            tbl.increments();
+            tbl.float("Quantity").notNullable();
+            tbl.integer("RecipeID")
+                .unsigned()
+                .references("Recipes.RecipeID")
+                .onDelete("RESTRICT")
+                .onUpdate("CASCADE");
+            tbl.integer("IngredientsID")
+                .unsigned()
+                .references("Ingredients.id")
+                .onDelete("RESTRICT")
+                .onUpdate("CASCADE");
+        })
+        .createTable("Steps", (tbl) => {
+            tbl.increments();
+            tbl.string("StepDescription", 255).notNullable();
+            tbl.integer("RecipeID")
+                .unsigned()
+                .references("Recipes.RecipeID")
+                .onDelete("RESTRICT")
+                .onUpdate("CASCADE");
         });
 };
 
 exports.down = function (knex) {
     return knex.schema
-        .dropTableIfExists("Recipes")
-        .dropTableIfExists("Ingredients");
+        .dropTableIfExists("Steps")
+        .dropTableIfExists("Recipe")
+        .dropTableIfExists("Ingredients")
+        .dropTableIfExists("Recipes");
 };
