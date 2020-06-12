447332da327544b9dddf3fc8312746578c0b340c
Michael Perez <mperez6473@gmail.com>
2020-06-11 20:37:26 -0500

    Added a recipesRouter and a db connecection file. Started adding in the recipes router bit.v

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 
 const server = express();
-
+const recipesRouter = require("./recipes/recipesRouter");
 server.use(express.json());
+server.use("/api/recipes", recipesRouter);
 
 module.exports = server;
